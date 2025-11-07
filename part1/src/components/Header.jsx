aa6dd0220357cefa2907446cc87e19582d1e0071
Adham Kandeel <adhamhkandeel27@gmail.com>
2025-11-07 18:38:06 +0200

    Exercise 1.5

--- part1/src/components/Header.jsx
+++ part1/src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Header = ({courseName}) => {
+const Header = ({course}) => {
   return (
     <div>
-        <h1>{courseName}</h1>
+        <h1>{course.name}</h1>
     </div>
   )
 }
