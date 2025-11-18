7b1c5a32834983b24e55be664e49b36a8e432ecd
Adham Kandeel <adhamhkandeel27@gmail.com>
2025-11-18 16:59:12 +0200

    exercise 2.2

--- part1_2/src/components/Content.jsx
+++ part1_2/src/components/Content.jsx
@@ -4,11 +4,21 @@ import Part from "./Part";
 const Content = (props) => {
   const { parts } = props;
   console.log(parts);
+
+  const TotalExercises = parts.reduce((acc, part) => {
+    return acc + part.exercises;
+  }, 0);
+
   return (
     <div>
       {parts.map((part, index) => {
         return <Part key={index} part={part} />;
       })}
+      <div className="total">
+        <p>
+          <strong>Total Exercises = {TotalExercises}</strong>
+        </p>
+      </div>
     </div>
   );
 };
--- part1_2/src/components/Course.jsx
+++ part1_2/src/components/Course.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import Header from './Header';
-import Content from './Content';
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
 
-
-function Course({course}) {
-    console.log(course);
+function Course({ course }) {
+  console.log(course);
   return (
     <section className="container">
-        <Header course ={course}/>
-        <Content parts = {course.parts}/>
+      <Header course={course} />
+      <Content parts={course.parts} />
     </section>
-    
-  )
+  );
 }
 
-export default Course
+export default Course;
